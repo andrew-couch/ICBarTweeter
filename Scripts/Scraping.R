593b9131815553da24ddd84ae5c39a62f2af775c
Andrew Couch <andrew.couch.ia@gmail.com>
2021-10-14 11:02:57 -0500

    Added date sorting

--- Scripts/Scraping.R
+++ Scripts/Scraping.R
@@ -5,7 +5,6 @@ suppressPackageStartupMessages({
   library(rvest)
 })
 
-
 # Helper function to look into detail of arrest and extract date of birth, was arrested, and officer name
 get_detail <- function(link){
   
@@ -29,6 +28,7 @@ read_html("https://www.iowa-city.org/IcgovApps/Police/ArrestBlotter") %>%
   mutate(details = paste0("https://www.iowa-city.org", details),
          details = map(details, get_detail)) %>% 
   unnest(details) %>% 
+  mutate(offense_date = lubridate::parse_date_time(offense_date, "%m/%d/%Y %h:%m:%s %p")) %>% 
   write_csv("E:/School/R Work/ICBarTweeter/Data/charge_history.csv", append = TRUE)
 
 message("Scraping Activity...")
@@ -37,13 +37,20 @@ read_html("https://www.iowa-city.org/IcgovApps/police/activitylog") %>%
   html_element("table") %>% 
   html_table() %>% 
   rename_all(~tolower(.x) %>% str_replace(" ", "_")) %>% 
+  mutate(offense_date = lubridate::parse_date_time(offense_date, "%m/%d/%Y %h:%m:%s %p")) %>% 
   write_csv("E:/School/R Work/ICBarTweeter/Data/police_activity.csv", append = TRUE)
 
 suppressMessages({
-  charge_hist <- read_csv("E:/School/R Work/ICBarTweeter/Data/charge_history.csv") %>% distinct()
-  police_activity <- read_csv("E:/School/R Work/ICBarTweeter/Data/police_activity.csv") %>% distinct()
+  charge_hist <- read_csv("E:/School/R Work/ICBarTweeter/Data/charge_history.csv") %>% 
+    distinct() %>% 
+    mutate(offense_date = lubridate::parse_date_time(offense_date, "%m/%d/%Y %h:%m:%s %p")) %>% 
+    arrange(offense_date)
+  
+  police_activity <- read_csv("E:/School/R Work/ICBarTweeter/Data/police_activity.csv") %>% 
+    distinct() %>% 
+    mutate(offense_date = lubridate::parse_date_time(offense_date, "%m/%d/%Y %h:%m:%s %p"))  %>% 
+    arrange(offense_date)
+  
   write_csv(charge_hist, "E:/School/R Work/ICBarTweeter/Data/police_activity.csv")
   write_csv(police_activity, "E:/School/R Work/ICBarTweeter/Data/police_activity.csv")
 })
-
-write_csv(charge_hist, "E:/School/R Work/ICBarTweeter/Data/police_activity.csv")
