64f9d2ce7e687a6058c5349affeb2ed9374213ce
Andrew Couch <andrew.couch.ia@gmail.com>
2021-10-20 13:42:44 -0500

    Update Tweeter.R

--- Scripts/Tweeter.R
+++ Scripts/Tweeter.R
@@ -1,7 +1,10 @@
 suppressPackageStartupMessages({
   library(tidyverse)
   library(rvest)
+  library(rtweet)
 })
+
+past_tweets <- read_csv("Data/tweeted_activity.csv")
 
 activity <- read_html("https://www.iowa-city.org/IcgovApps/Police/ActivityLog") %>% 
   html_element(".body-content") %>% 
@@ -9,6 +12,7 @@ activity <- read_html("https://www.iowa-city.org/IcgovApps/Police/ActivityLog") 
   rename_with(~tolower(.x) %>% str_replace_all(" ", "_")) %>% 
   mutate(link = paste0("https://www.iowa-city.org/IcgovApps/Police/Details?dispatchNumber=", dispatch_number)) %>% 
   filter(activity == "BAR CHECK") %>% 
+  # filter(!(dispatch_number %in% past_tweets$dispatch_number)) %>% 
   mutate(link = map(link, read_html),
          dispatch_time = map_chr(link, ~html_element(.x, "dd:nth-child(4)") %>% html_text()),
          activity_medium = map_chr(link, ~html_element(.x, "dd:nth-child(8)") %>% html_text()),
@@ -22,5 +26,24 @@ if(nrow(activity) == 0){
   message("No Bar Raids")
 } else {
   activity
-}
+  write_csv(activity, file = "Data/tweeted_activity.csv", append = TRUE)
+  # Tweet activity
+  # create_token(
+  #   app = "bartweeter",  
+  #   consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  #   consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  #   access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  #   access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+  # )
   
+  # activity %>% 
+  #   walk2(location, dispatch_time, .f = function(location, time){
+  #     post_tweet(status = paste0(str_to_title(location), " was just raided at ", time))
+  #   })
+}
+# TWITTER_CONSUMER_API_KEY: ${{ secrets.TWITTER_CONSUMER_API_KEY }}
+# TWITTER_CONSUMER_API_SECRET: ${{ secrets.TWITTER_CONSUMER_API_SECRET }}
+# TWITTER_ACCESS_TOKEN: ${{ secrets.TWITTER_ACCESS_TOKEN }}
+# TWITTER_ACCESS_TOKEN_SECRET: ${{ secrets.TWITTER_ACCESS_TOKEN_SECRET }}
+
+
